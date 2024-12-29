99c87f8d228cca38290c9b915b418281afca3f4e
guilhermeoliveira-devjourneylog <dev_guilherme_oliveira@outlook.com>
2024-12-29 20:52:25 +0000

    update 1.1.3

--- UserManagementAPI/Program.cs
+++ UserManagementAPI/Program.cs
@@ -84,10 +84,14 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "User Management API v1");
+        options.RoutePrefix = string.Empty; // Configura o Swagger na raiz "/"
+    });
 }
 
 // Middleware for error handling
